99cd212355c5b1c14e35ad9954b57f1c966625cb
Stefan Gehrig <gehrig@teqneers.de>
2015-07-21 18:29:11 +0200

    initial setup to move unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * teqneers/ext-application
+ *
+ * @category   TQ
+ * @package    TQ\ExtJS\Application
+ * @copyright  Copyright (C) 2015 by TEQneers GmbH & Co. KG
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
